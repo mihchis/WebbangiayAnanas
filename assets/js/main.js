84d861a5d7b8f2f2129d16300a4112980cd014fd
mihchis <trin79136@gmail.com>
2021-12-24 19:24:48 +0700

    .
    .

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	
+	$('.slide').slick({
+		slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        autoplay: true,
+	});
+	
+});
